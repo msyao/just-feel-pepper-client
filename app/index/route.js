f1130acb2c752c177d2a8762602216ee7e0fd3ff
msyao-github <msyaois@gmail.com>
2016-04-19 18:08:40 -0400

    edit indext template & route to have sign in work

--- app/index/route.js
+++ app/index/route.js
@@ -1,4 +1,20 @@
+// index/route.js
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  auth: Ember.inject.service(),
+  flashMessages: Ember.inject.service(),
+
+  actions: {
+    signIn (credentials) {
+      return this.get('auth').signIn(credentials)
+      .then(() => this.transitionTo('posts'))
+      // .then(() => this.transitionTo('application'))
+      .then(() => this.get('flashMessages').success('Thanks for signing in!'))
+      .catch(() => {
+        this.get('flashMessages')
+        .danger('There was a problem. Please try again.');
+      });
+    },
+  },
 });
